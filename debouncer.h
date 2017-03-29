f11b62237c2a73d74207dc071f6739b47e6a03cf
Witloofboer <cedric.busschots@gmail.com>
2017-03-29 11:15:18 +0200

    Added prototype of the function to the header.

--- debouncer.h
+++ debouncer.h
@@ -22,10 +22,6 @@
 #define PRESS_MSEC 10           // For pressed, wait 10 msec stable
 #define RELEASE_MSEC 100        // For release, wait 100 msec stable
 
-// Create new datatype to keep track of the current state of the debouncer.
-typedef enum debounce_stat {
-    debounce_stat_ok          = 0x0,
-    debounce_stat_initialized = 0x1,
-} debounce_stat;
+void debouncer(uint8_t *changed, uint8_t *pressed);
 
 #endif
